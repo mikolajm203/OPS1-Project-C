8102d1d152c1c82a6f812bdba4655b5b4caecab6
Mikołaj Stańczyk <62181873+mikolajm203@users.noreply.github.com>
2020-12-12 13:06:57 +0000

    Update map.c

--- src/map.c
+++ src/map.c
@@ -1,6 +1,8 @@
 #include "map.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int addRoom(Map* m, int ID){
     Room* toAdd =(Room*)malloc(sizeof(Room));
@@ -30,11 +32,19 @@ int addDoor(Map* m, int ID1, int ID2){
     Room* tmp = m->first;
     while(tmp != NULL){
         if(tmp->ID == ID1){
+            for(int i = 0; i < tmp->noOfDoors; i++){
+                if(tmp->doors[i] == ID2)
+                    return 0;
+            }
             tmp->doors[tmp->noOfDoors] = ID2;
             tmp->noOfDoors++;
             isFound++;
         }
         if(tmp->ID == ID2){
+            for(int i = 0; i < tmp->noOfDoors; i++){
+                if(tmp->doors[i] == ID1)
+                    return 0;
+            }
             tmp->doors[tmp->noOfDoors] = ID1;
             tmp->noOfDoors++;
             isFound++;
@@ -52,8 +62,12 @@ void printRoom(Room r){
     printf("\n");
 }
 void printMap(Map m){
-    Room* tmp = m.first;
     printf("------MAP------\n");
+    if(m.first == NULL){
+        printf("empty map\n");
+        return;
+    }
+    Room* tmp = m.first;
     while(tmp != NULL){
         printRoom(*tmp);
         tmp = tmp->next;
@@ -74,10 +88,51 @@ void deleteMap(Map m){
     free(cur);
     return;
 }
-Map loadRandom(int n){
-    Map ret = {NULL, 0};
+Map* loadRandom(int n){
+    Map* ret = (Map*)malloc(sizeof(Map));
+    if (ret == NULL)    return NULL;
     for(int i = 0; i < n; i++){
 
     }
     return ret;
+}
+int loadFromFile(char* path, Map* map){
+    map->first = NULL;
+    map->noOfRooms = 0;
+    int f;
+    if((f = open(path, O_RDONLY)) < 0)    return -1;
+    int buf;
+    int noOfRooms;
+    if(read(f, &noOfRooms, sizeof(int)) == -1) return -1;
+    for(int i = 0; i < noOfRooms; i++){
+        if(read(f, &buf, sizeof(int)) == -1) return -1;
+        if(addRoom(map, buf) < 0) return -1;
+    }
+    int connectedRooms[2];
+    while(read(f, &connectedRooms[0], sizeof(int)) != 0){
+        if(read(f, &connectedRooms[1], sizeof(int)) == -1) return -1;
+        if(addDoor(map, connectedRooms[0], connectedRooms[1]) < 0) return -1;
+    }
+    close(f);
+    return 0;
+}
+int saveMap(Map m, char* path){
+    int f;
+    if((f = open(path, O_WRONLY|O_CREAT, 0777)) < 0) return -1;
+    if(write(f, &m.noOfRooms, sizeof(int)) == -1) return -1;
+    Room* tmp = m.first;
+    while(tmp != NULL){
+        if(write(f, &tmp->ID, sizeof(int)) == -1) return -1;
+        tmp = tmp->next;
+    }
+    tmp = m.first;
+    while(tmp != NULL){
+        for(int i = 0; i < tmp->noOfDoors; i++){
+            if(write(f, &tmp->ID, sizeof(int)) == -1) return -1;
+            if(write(f, &tmp->doors[i], sizeof(int)) == -1) return -1;
+        }
+        tmp = tmp->next;
+    }
+    close(f);
+    return 0;
 }
