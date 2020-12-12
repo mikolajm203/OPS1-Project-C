5b054372a87d0bf407cde5a3c777bbc632b4b26e
Mikołaj Stańczyk <62181873+mikolajm203@users.noreply.github.com>
2020-12-12 13:06:16 +0000

    Update map.h

--- src/map.h
+++ src/map.h
@@ -14,13 +14,14 @@ struct Map_{
     Room* first;
     int noOfRooms;
 };
-Map loadFromFile(char* file);                   // Returns a map loaded from a file.
-Map loadFromDir(char* path_d, char* path_f);    /* generates a map that mirrors a directory structure of path_d and its subdirectories.
+int loadFromFile(char* path, Map* map);                  // Returns a map loaded from a file. Returns NULL on failure.
+Map* loadFromDir(char* path_d, char* path_f);   /* generates a map that mirrors a directory structure of path_d and its subdirectories.
                                                 Every directory is a room and the rooms are connected with their parent directories and subdirectories.
                                                 The created map is written to path-f. */
-Map loadRandom(int n);              // Returns a random map of n rooms and writes the result in path.
+Map* loadRandom(int n);                         // Returns a random map of n rooms and writes the result in path.
+int saveMap(Map m, char* path);                 // Saves map to the path. Returns 0 on succes and -1 otherwise
 int addRoom(Map* m, int ID);                    // Adds a room to the Map. Returns 0 on succesful addition
-int addDoor(Map* m, int ID1, int ID2);          // Adds a door to the Room. Returns 0 on succesful addition
+int addDoor(Map* m, int ID1, int ID2);          // Adds a door to the Room. Returns 0 on succesful addition. Does nothing if door already exists
 void printRoom(Room r);                         // Prints info about all the Rooms in the Map
 void printMap(Map m);                           // Prints info about the Room
 void deleteMap(Map m);                          // Frees all allocated memory
