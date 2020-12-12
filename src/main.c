749dd3422c8f8eebaa9efc5bff35b068b0bdc9f6
Mikołaj Stańczyk <62181873+mikolajm203@users.noreply.github.com>
2020-12-12 20:37:55 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,19 +3,20 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "map.h"
-#include "controls.h"
+#include "Game.h"
+#include "Input.h"
 #define ERR(source) (perror(source),\
                      fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
                      exit(EXIT_FAILURE))
 
+
 int main(int argc, char** argv){
-    Map map = {NULL, 0};
-    GAME_STATE state = MAIN_MENU;
+    Game game;
+    game.state = MAIN_MENU;
     while(1){
+        printInfo(&game);
         char* command = readInput();
-        proccessInput(&state, command, &map);
+        proccessInput(command, &game);
     }
-    printMap(map);
     return EXIT_SUCCESS;
 }
