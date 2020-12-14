7b6f5a46f4bb621222f235ec9c609247ad56e1c4
Mikołaj Stańczyk <62181873+mikolajm203@users.noreply.github.com>
2020-12-14 23:42:51 +0000

    Update Game.h

--- src/Game.h
+++ src/Game.h
@@ -1,12 +1,24 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-#define MAX_DOORS 1000
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#define ERR(source) (perror(source),\
+                     fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
+                     exit(EXIT_FAILURE))
+#define MAX_DOORS 50
 #define MAX_ITEMS 2
 #define MAX_ROOMS 50
+#define IN_BACKPACK 1000
 
 typedef enum {
     MAIN_MENU,
-    IN_GAME
+    IN_GAME,
+    POST_GAME
 }GAME_STATE;
 
 typedef struct Item_ Item;
@@ -47,24 +59,39 @@ struct Game_{
     Item* items;
     int noOfItems;
 };
+typedef struct SeekArg_{                        // struct passed to function seekPath
+    pthread_t tid;
+    int* pathLength;
+    pthread_mutex_t* mxLength;
+    int* path;
+    pthread_mutex_t* mxPath;
+    unsigned int seed;
+    int destID;
+    Player player;
+    Map* map;
+}SeekArg;
 
-void initGame(Game* game); 
-int loadFromFile(char* path, Map* map);         // Returns a map loaded from a file. Returns NULL on failure.
-Map* loadFromDir(char* path_d, char* path_f);   /* generates a map that mirrors a directory structure of path_d and its subdirectories.
-                                                Every directory is a room and the rooms are connected with their parent directories and subdirectories.
-                                                The created map is written to path-f. */
-Map* loadRandom(int n);                         // Returns a random map of n rooms and writes the result in path.
-int saveMap(Map m, char* path);                 // Saves map to the path. Returns 0 on succes and -1 otherwise
-int addRoom(Map* m, int ID);                    // Adds a room to the Map. Returns 0 on succesful addition
-int addDoor(Map* m, int ID1, int ID2);          // Adds a door to the Room. Returns 0 on succesful addition. Does nothing if door already exists
-void printRoom(Room r);                         // Prints info about all the Rooms in the Map
-void printMap(Map m);                           // Prints info about the Room
-void deleteMap(Map* m);                         // Frees all allocated memory
-int putItem(Map* m, int roomID, int itemID);
-int setDestined(Map* map, int roomID,int itemID);
-void initPlayer(Player* player, Map* map);
-Item* initItems(Map* m);                       // Populates map with items
-void moveTo(Map* map, Player* player,int room);
-void pickUp(Player* player, Item* items, int noOfItems, int itemToPickUp);
-void drop(Player* player, Item* items, int noOfItems, int itemToDrop);
+void loadFromFile(char* path, Map* map);                                    // loads map from file and starts the game
+void generateMap(int n, char* path);                                        // generates random map and saves it in path
+Map* loadFromDir(char* path_d, char* path_f);                               /* generates a map that mirrors a directory structure of path_d and its subdirectories.
+                                                                            Every directory is a room and the rooms are connected with their parent directories and
+                                                                            subdirectories. The created map is written to path-f. */
+void saveMap(Map m, char* path);                                            // Saves map to the path.
+void saveGame(Game game, char* path);                                       // 
+void addRoom(Map* m, int ID);                                               // Adds a room to the Map.
+void addDoor(Map* m, int ID1, int ID2);                                     // Adds a door to the Room. Does nothing if door already exists.
+void printRoom(Room r);                                                     // Prints info about the Room
+void printMap(Map m);                                                       // Prints info about all the Rooms in the Map
+Item* initItems(Map* m);                                                    // Populates map with items
+int putItem(Map* m, int roomID, int itemID);                                // Returns true if item can be placed in roomID.
+int setDestined(Map* map, int roomID,int itemID);                           // Returns true it item's destination can be roomID
+void initPlayer(Player* player, Map* map);                                  // Initializes Player struct. Puts him in random room
+void initGame(Game* game);                                                  // Initializes Gamestruct
+void moveTo(Map* map, Player* player,int room);                             // Moves player to room
+void pickUp(Player* player, Item* items, int noOfItems, int itemToPickUp);  // Picks up itemToPickUp if possible
+void drop(Player* player, Item* items, int noOfItems, int itemToDrop);      //Drops itemToDrop if possible
+void printPath(Game game, int room, int k);                                 //Prints moderately short path to room using k threads
+void* seekPath(void* args);                                                 //Function for threads in printPath
+int checkWin(Item* items, int noOfItems);                                   //checks if player has put items in correct rooms
+void cleanup(Game* game);
 #endif
