07a93a90e299fedb736a069d6d2687b014da7a4f
Mikołaj Stańczyk <62181873+mikolajm203@users.noreply.github.com>
2021-03-02 13:21:52 +0100

    Update Game.h

--- src/Game.h
+++ src/Game.h
@@ -1,12 +1,5 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <pthread.h>
 #define ERR(source) (perror(source),\
                      fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
                      exit(EXIT_FAILURE))
@@ -14,6 +7,19 @@
 #define MAX_ITEMS 2
 #define MAX_ROOMS 50
 #define IN_BACKPACK 1000
+#define _GNU_SOURCE
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <signal.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+
 
 typedef enum {
     MAIN_MENU,
@@ -23,41 +29,42 @@ typedef enum {
 
 typedef struct Item_ Item;
 struct Item_{                                   //Item struct to store all neccessary data
-    int ID;                 //item ID
-    int currentRoom;        //room ID where that item lies
-    int destinedRoom;         //room ID where that item belongs
+    int ID;                 
+    int currentRoom;        
+    int destinedRoom;      
 };
 
 typedef struct Room_ Room;                      // Representation of a Room with adjacency list(int doors[MAX_DOORS])
 struct Room_{                       
-    int ID;                 //ID of the room
-    int curItems[MAX_ITEMS];//array of items' ID in the room
-    int noOfCurItems;       //number of items in the room
-    int noOfDestinedItems;  //number of items that come from this room
-    int doors[MAX_DOORS];   //array of rooms' IDs connected to this room
-    int noOfDoors;          //Number of dors in this room
+    int ID;                 
+    int curItems[MAX_ITEMS];
+    int noOfCurItems;       
+    int noOfDestinedItems;  
+    int doors[MAX_DOORS];   
+    int noOfDoors;          
 };
 
 typedef struct Player_ Player;                  //Player struct to store all neccessary data
 struct Player_{
-    int backpack[MAX_ITEMS];//array of items' IDs in player's backpack
-    int noOfItems;          //number of items in the backpack
-    Room* currentRoom;        //room ID where player is right now
+    int backpack[MAX_ITEMS];
+    int noOfItems;          
+    Room* currentRoom;      
 };
 
-typedef struct Map_ Map;                        // Linked list of rooms(our graph)
-struct Map_{                        
+typedef struct Map_ Map;                        // Graph representation of a map
+struct Map_{
+    int noOfRooms;                        
     Room rooms[MAX_ROOMS];
-    int noOfRooms;
 };
 
-typedef struct Game_ Game;
+typedef struct Game_ Game;                      
 struct Game_{
     Map map;
     GAME_STATE state;
     Player player;
-    Item* items;
     int noOfItems;
+    Item* items;
+    pthread_mutex_t* mxGame;
 };
 typedef struct SeekArg_{                        // struct passed to function seekPath
     pthread_t tid;
@@ -70,14 +77,17 @@ typedef struct SeekArg_{                        // struct passed to function see
     Player player;
     Map* map;
 }SeekArg;
-
+ssize_t bulk_write(int fd, void *buf, size_t count);
+ssize_t bulk_read(int fd, void *buf, size_t count);
 void loadFromFile(char* path, Map* map);                                    // loads map from file and starts the game
 void generateMap(int n, char* path);                                        // generates random map and saves it in path
-Map* loadFromDir(char* path_d, char* path_f);                               /* generates a map that mirrors a directory structure of path_d and its subdirectories.
+void loadFromDir(Map* m, char* path_d, char* path_f);                       // saves ma generated from path_d to path_f
+void DirToMap(Map* m, char* path_d, int prevID);                            /* generates a map that mirrors a directory structure of path_d and its subdirectories.
                                                                             Every directory is a room and the rooms are connected with their parent directories and
-                                                                            subdirectories. The created map is written to path-f. */
-void saveMap(Map m, char* path);                                            // Saves map to the path.
-void saveGame(Game game, char* path);                                       // 
+                                                                            subdirectories.*/
+void saveMap(Map* m, char* path);                                           // Saves map to the path.
+void saveGame(Game game, char* path);                                       // Saves game to the path
+void loadGame(Game* game, char* path);                                      // Loads game from the path
 void addRoom(Map* m, int ID);                                               // Adds a room to the Map.
 void addDoor(Map* m, int ID1, int ID2);                                     // Adds a door to the Room. Does nothing if door already exists.
 void printRoom(Room r);                                                     // Prints info about the Room
@@ -92,6 +102,6 @@ void pickUp(Player* player, Item* items, int noOfItems, int itemToPickUp);  // P
 void drop(Player* player, Item* items, int noOfItems, int itemToDrop);      //Drops itemToDrop if possible
 void printPath(Game game, int room, int k);                                 //Prints moderately short path to room using k threads
 void* seekPath(void* args);                                                 //Function for threads in printPath
+void swapItems(Game* g, int ID1, int ID2);                                  //Swaps location of items ID1 and ID2
 int checkWin(Item* items, int noOfItems);                                   //checks if player has put items in correct rooms
-void cleanup(Game* game);
 #endif
